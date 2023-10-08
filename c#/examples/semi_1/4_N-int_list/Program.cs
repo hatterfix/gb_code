e3d12f0cc3c8ae132b0d04c5eceab27fbaf8c1f0
Kirill Krivospitskiy <hatter@complat.ru>
2023-10-08 19:25:33 +0300

    add 4th task - N int list

---
+++ c#/examples/semi_1/4_N-int_list/Program.cs
@@ -0,0 +1,11 @@
+﻿// Код принимает на вход целочисленное N и выводит список чисел от -N до N:
+
+Console.Write ("Введите  целое число: ") ;
+int N = Convert.ToInt32(Console.ReadLine()) ;
+
+int M = N * -1 ;
+while (N != M)
+{
+Console.Write( "  " + M ) ;
+M++ ;
+}
