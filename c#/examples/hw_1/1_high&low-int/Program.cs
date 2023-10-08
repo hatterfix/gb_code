97b90ad1f498fa1716bc6e38d9b524922f9413dc
Kirill Krivospitskiy <hatter@complat.ru>
2023-10-09 01:14:01 +0300

    add 1st hw task:

---
+++ c#/examples/hw_1/1_high&low-int/Program.cs
@@ -0,0 +1,19 @@
+﻿// Код, который на вход принимает два числа
+//  и выводит, какое число большее, а какое меньшее.
+Console.Write ("Введите первое целое число: ") ;
+int firstNumber = Convert.ToInt32(Console.ReadLine()) ;
+Console.Write ("Введите второе целое число: ") ;
+int secondNumber = Convert.ToInt32(Console.ReadLine()) ;
+
+if ( firstNumber == secondNumber) 
+{
+    Console.WriteLine($"Введенные числа равны `{secondNumber}`") ;
+}
+if ( firstNumber > secondNumber) 
+{
+    Console.WriteLine($" Первое число `{firstNumber}`  больше чем второе число `{secondNumber}`") ;
+}
+if ( firstNumber < secondNumber) 
+{
+    Console.WriteLine($"Первое число `{firstNumber}` меньше чем второе число `{secondNumber}`") ;
+}
