221160c3b511d4de16ba97ff2b01490e3806b27c
Kirill Krivospitskiy <hatter@complat.ru>
2023-10-08 18:09:58 +0300

    change weekday check to else if cycle:

--- c#/examples/semi_1/3_weekday/Program.cs
+++ c#/examples/semi_1/3_weekday/Program.cs
@@ -1,39 +1,36 @@
 ﻿// Код выдает название дня недели ро заданному номеру :
 Console.Write ("Введите номер дня недели: ") ;
 int number = Convert.ToInt32(Console.ReadLine()) ;
-if ( number < 1) 
-{
-    Console.WriteLine("Ой-Ой... Дни недели считаются с 1-го:") ;
-}
+
 if ( number == 1) 
 {
     Console.WriteLine($"{number}-й день недели - понедельник") ;
 }
-if ( number == 2) 
+else if ( number == 2) 
 {
     Console.WriteLine($"{number}-й день недели - вторник") ;
 }
-if ( number == 3) 
+else if ( number == 3) 
 {
     Console.WriteLine($"{number}-й день недели - среда") ;
 }
-if ( number == 4) 
+else if ( number == 4) 
 {
     Console.WriteLine($"{number}-й день недели - четверг") ;
 }
-if ( number == 5) 
+else if ( number == 5) 
 {
     Console.WriteLine($"{number}-й день недели - пятница") ;
 }
-if ( number == 6) 
+else if ( number == 6) 
 {
     Console.WriteLine($"{number}-й день недели - суббота, ура !!!") ;
 }
-if ( number == 7) 
+else if ( number == 7) 
 {
     Console.WriteLine($"{number}-й день недели - воскоесенье, уоа !!!") ;
 }
-if ( number > 7) 
+else
 {
     Console.WriteLine("Ой-ой... В неделе только 7 дней.") ;
 }
