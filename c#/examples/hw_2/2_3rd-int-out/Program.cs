5b0e41a8d87d8463edc5cac64d7a119ffa900f39
hatter <hatter@complat.ru>
2023-10-10 18:45:19 +0300

    add 2nd task - get 3rd int:

---
+++ c#/examples/hw_2/2_3rd-int-out/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Код выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write ("Введите целое число: ") ;
+int number = Convert.ToInt32(Console.ReadLine()) ;
+
+while ( number > 1000 ) 
+{
+    number = number / 10 ;
+}
+
+if ( 99 < number && number < 1000) 
+{
+    Console.WriteLine($"{number%10} - третья цифра в числе") ;
+}
+else 
+{
+    Console.WriteLine($"Ошибка - в числе {number} нет 3-й цифры.") ;
+}
+
