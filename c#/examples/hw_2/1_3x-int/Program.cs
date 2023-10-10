17061d329641b4358ff2e462273664f220771445
hatter <hatter@complat.ru>
2023-10-10 17:41:46 +0300

    add hw_2 branch & first task:

---
+++ c#/examples/hw_2/1_3x-int/Program.cs
@@ -0,0 +1,12 @@
+﻿// Код принимает на вход трёхзначное число и выводит вторую цифру в нём
+Console.Write ("Введите трёхзначное число: ") ;
+int number = Convert.ToInt32(Console.ReadLine()) ;
+
+if ( 99 < number && number < 1000) 
+{
+    Console.WriteLine($"{(number / 10)%10} - вторая цифра в числе {number}") ;
+}
+else 
+{
+    Console.WriteLine("Ошибка - Число не трёхзначное") ;
+}
