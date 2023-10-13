802b48164d408a0de2844950408ea44e883b8614
Kirill Krivospitskiy <hatter@complat.ru>
2023-10-13 13:12:58 +0300

    add 1st task 5x-int for hw3:

---
+++ c#/examples/hw_3/1_int-5x/Program.cs
@@ -0,0 +1,31 @@
+﻿// роверить является ли число пятизначным, 
+// в противном случае - вывести 
+// Число не пятизначное и False в следующей строке.
+Console.Write ("Введите пятизначное число: ") ;
+
+int number = Convert.ToInt32(Console.ReadLine()) ;
+bool result = number >= 10000 && number <= 99999;
+
+if ( number > 9999 && number < 100000) 
+{
+    Console.WriteLine($"{result}") ;
+}
+else 
+{
+    Console.WriteLine("Число не пятизначное") ;
+    Console.WriteLine ($"{result}") ;
+}
+
+
+//Код для автотеста:
+// bool result = number >= 10000 && number <= 99999;
+
+// if ( number > 9999 && number < 100000) 
+// {
+//     return result;
+// }
+// else 
+// {
+//     Console.WriteLine("Число не пятизначное") ;
+//     return result; 
+// }
