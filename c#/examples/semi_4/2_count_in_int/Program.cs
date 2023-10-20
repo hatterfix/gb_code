fbbae500fec3a1731f53002ec58d0b9ebe8b35a9
Kirill Krivospitskiy <hatter@complat.ru>
2023-10-20 21:58:44 +0300

    add int count check with string:

---
+++ c#/examples/semi_4/2_count_in_int/Program.cs
@@ -0,0 +1,8 @@
+﻿// Код принимает на взод число и выводит количество цифр в нём
+
+Console.Write ("Введите целое число: ") ;
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine())) ;
+
+// 1 способ - с использованием строк
+string strNum = Convert.ToString(number) ;
+Console.WriteLine ($"Количкство символов в числе = {strNum.Length}") ;
