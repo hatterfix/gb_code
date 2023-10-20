9ec90a894f618538bd030151831f41eb91e1a8e3
Kirill Krivospitskiy <hatter@complat.ru>
2023-10-20 20:56:31 +0300

    add ReadInt func example:

---
+++ c#/examples/semi_4/1.1_sum_to_N+ReadIn/Program.cs
@@ -0,0 +1,41 @@
+﻿// Код выводит сумму чисел от 1 до N,
+// С помощью функции SumNumbers()
+
+// Описываем функцию, 
+//Берет заданное число, считает сумму от 1 до числа и возвращает результат
+
+int SumNumbers(int a)
+{
+	int sum = 0 ;
+	
+	for (int i =1; i <= a ; i++)
+	{
+	sum += i;
+	}
+
+	return sum ; //оператор перехода - принудительно завершает функцию и возвращает значение 
+}
+
+int ReadInt(string text)
+{
+    Console.Write (text) ;
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+//---------------------------------------------------------
+
+
+int number = ReadInt("Введите число N: ") ;
+
+Console.WriteLine($"Сумма чисел от 1 до N = {SumNumbers(number)}"); //Выводим результат функции SumNumbers
+
+Console.WriteLine($"---------------------------------------------");
+
+Console.WriteLine($"Сумма чисел от 1 до N = {SumNumbers(5)}");
+
+Console.WriteLine($"---------------------------------------------");
+
+Console.WriteLine($"Сумма чисел от 1 до N = {SumNumbers(7)}");
+
+Console.WriteLine($"---------------------------------------------");
+
+Console.WriteLine($"Сумма чисел от 1 до N = {SumNumbers(9)}");
