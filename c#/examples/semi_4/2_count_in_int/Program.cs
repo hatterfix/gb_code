fc4f5d4a74d2dace7fe72761873ade6ae38ac8ae
Kirill Krivospitskiy <hatter@complat.ru>
2023-10-21 01:33:06 +0300

    add func var in 2nd task:

--- c#/examples/semi_4/2_count_in_int/Program.cs
+++ c#/examples/semi_4/2_count_in_int/Program.cs
@@ -1,19 +1,45 @@
 ﻿// Код принимает на взод число и выводит количество цифр в нём
 
-Console.Write ("Введите целое число: ") ;
-int number = Math.Abs(Convert.ToInt32(Console.ReadLine())) ;
+//3 способ - применение функций:
 
-// 1 способ - с использованием строк
-string strNum = Convert.ToString(number) ;
-Console.WriteLine ($"Количество символов в числе = {strNum.Length}") ;
+int ReadInt(string text)
+{
+    Console.Write (text) ;
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
 
 
-//2 способ - деление на 10
+int LenNumber(int value)
+{
 int len = 0;
 
-while (number > 0)
-{
-    number/=10 ;
+    while (value > 0)
+    {
+    value/=10 ;
     len++;
+    }
+    return len;
 }
-Console.WriteLine($"Количество символов в числе = {len}") ;
+//---------------------------------------------------------
+
+
+int number = ReadInt("Введите число: ") ;
+
+Console.WriteLine ($"Количество символов в числе = {LenNumber(number)}") ;
+
+
+
+// // 1 способ - с использованием строк
+// string strNum = Convert.ToString(number) ;
+// Console.WriteLine ($"Количество символов в числе = {strNum.Length}") ;
+
+
+// //2 способ - деление на 10
+// int len = 0;
+
+// while (number > 0)
+// {
+//     number/=10 ;
+//     len++;
+// }
+// Console.WriteLine($"Количество символов в числе = {len}") ;
