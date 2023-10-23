a360e51c48dbf0fbc39f3e2f05335f7a9d4439c3
Kirill Krivospitskiy <hatter@complat.ru>
2023-10-23 21:38:08 +0300

    add hw_5 1st autotest

---
+++ c#/examples/hw_5/autotests/1_mass_count-elements/Program.cs
@@ -0,0 +1,25 @@
+﻿// Код, который задаёт массив из 8 элементов и выводит их на экран.
+
+// Код для автотеста с методами включительно
+
+// public static int CountEvenElements(int[] array)
+// { 
+//     // Введите свое решение ниже
+// int count = 0;
+// for(int i = 0; i < array.Length; i++)
+//     {
+//     if(array[i]%2 == 0) count++;
+//     }
+//     return count;
+
+// }
+
+// public static void PrintArray(int[] array)
+// {
+//     // Введите свое решение ниже
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         System.Console.Write(array[i] + "\t");
+//     }
+//     System.Console.WriteLine();
+// }
