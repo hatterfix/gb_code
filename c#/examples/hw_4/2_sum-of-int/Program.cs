8f8aa66f5843b4a61ce1a97d5189f7242d48324d
Kirill Krivospitskiy <hatter@complat.ru>
2023-10-23 14:04:24 +0300

    add hw_4 2nd task SumOfInt:

---
+++ c#/examples/hw_4/2_sum-of-int/Program.cs
@@ -0,0 +1,28 @@
+﻿int ReadInt(string text)
+{
+    Console.Write (text) ;
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+
+int SumOfDigits(int number)
+    {
+        int sum = 0;
+        int remind;
+
+        while (number > 0)
+        {
+            remind = number % 10;
+            sum += remind;
+            number /= 10;
+        }
+
+        return sum;
+    }   
+//---------------------------------------------------------
+
+int number = ReadInt("Введите число: ") ;
+
+int sum = SumOfDigits(number);
+
+Console.WriteLine($"Скмма цифр в числе {number} = {sum}");
