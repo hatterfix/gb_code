780b08e442337ee3041ef38db9ff3d459b7224d4
Kirill Krivospitskiy <hatter@complat.ru>
2023-10-23 16:37:46 +0300

    add 3rd task massiv-8x

---
+++ c#/examples/hw_4/3_mass-x8/Program.cs
@@ -0,0 +1,26 @@
+﻿// Код, который задаёт массив из 8 элементов и выводит их на экран.
+
+int[] CreateArray()
+       {
+        int[] numbers = new int[8]; // Создаем массив из 8 элементов
+
+        // Заполняем массив введеггыми числами
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write($"Введите число для элемента {i + 1}: ");
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        return numbers;
+    }
+//---------------------------------------------------------
+
+        int[] numbers = CreateArray(); // Вызов функции для создания массива
+
+        // Выводим массив с табуляцией , через запятую
+        Console.WriteLine("Элементы массива:");
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write($"{numbers[i] + "," + "\t"}");
+        }
+        
