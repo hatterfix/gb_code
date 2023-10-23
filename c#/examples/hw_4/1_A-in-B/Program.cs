b4ae4d2adf94f173d3031afe1d754effc3af1ba1
Kirill Krivospitskiy <hatter@complat.ru>
2023-10-23 13:52:35 +0300

    add hw_4 1st task A-to-B

---
+++ c#/examples/hw_4/1_A-in-B/Program.cs
@@ -0,0 +1,32 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+
+
+int ReadInt(string text)
+{
+    Console.Write (text) ;
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+
+int Power(int A, int B)
+    {
+        int result = 1;
+
+        for (int i = 1; i <= B; i++)
+        {
+            result *= A;
+        }
+        return result;
+    }   
+//---------------------------------------------------------
+
+int numA = ReadInt("Введите число A: ") ;
+
+int numB = ReadInt("Введите число B: ") ;
+
+
+int result = Power(numA, numB);
+
+Console.WriteLine($"Результат {numA} в степени {numB}: {result}");
+
