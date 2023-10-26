a0c92a2e96b63826ca9d805178deabc3d4781d91
Kirill Krivospitskiy <hatter@complat.ru>
2023-10-27 01:19:26 +0300

    add 2nd task reverse_array

---
+++ c#/examples/semi_5/2_reverse_array/Program.cs
@@ -0,0 +1,37 @@
+﻿//Код меняет положительные элементы массива на отрицательные
+// и наоборот
+int[] generateArray (int sizeArray , int leftRange , int rightRange)
+{
+int[] newArray = new int[sizeArray] ;
+Random rand = new Random() ;
+
+// Цикл для заполнения массива случайными числами из указанного диапазона
+
+for (int i = 0 ; i < newArray.Length ; i++)
+    {
+        newArray[i] = rand.Next(leftRange , rightRange + 1) ; // т к rand не обработает правое значение диапазона
+    }
+    return newArray ;
+}
+
+
+void intArrayPrint(int[] arrayForPrint)
+        {
+
+            System.Console.WriteLine("[" + string.Join(", ", arrayForPrint) + "]");
+        }
+
+void ChangeArray(int[] array)
+{
+        for (int i = 0 ; i < array.Length ; i++)
+        {
+            array[i] = -array[i];
+        }
+}
+    //------------------
+
+int[] array = generateArray(10 , -9 ,9) ;
+intArrayPrint(array);
+
+ChangeArray(array);
+intArrayPrint(array);
