18410f48a9f7d8313c7837d82b0cf3b3ef6e3e03
Kirill Krivospitskiy <hatter@complat.ru>
2023-10-27 02:11:40 +0300

    add 3rd task find element in array:

---
+++ c#/examples/semi_5/3_check-element_in-array/Program.cs
@@ -0,0 +1,56 @@
+﻿// Код ищет присутствует ли заданный элемент в массиве.
+int[] generateArray (int sizeArray , int leftRange , int rightRange)
+{
+int[] newArray = new int[sizeArray] ;
+Random rand = new Random() ;
+
+// Цикл для заполнения массива случайными числами из указанного диапазона
+
+for (int i = 0 ; i < newArray.Length ; i++)
+    {
+        newArray[i] = rand.Next(leftRange , rightRange + 1) ; // т к rand не обработает правое значение диапазона
+    }
+    return newArray ;
+}
+
+
+void intArrayPrint(int[] arrayForPrint)
+        {
+
+            System.Console.WriteLine("[" + string.Join(", ", arrayForPrint) + "]");
+        }
+
+int ReadInt(string text)
+{
+System.Console.Write(text) ;
+return Convert.ToInt32(Console.ReadLine()) ;
+}
+
+bool FindElement(int[] array, int element)
+{
+for (int i = 0 ; i < array.Length ; i++)
+    if (array[i] == element) return true; // Сокращам синтаксис записи цикла
+
+    return false ;
+}   
+
+
+ //------------------
+
+int[] array = generateArray(10 , -9 ,9) ;
+intArrayPrint(array);
+
+int number = ReadInt("Введите искомое чтсло: ");
+
+bool key = FindElement(array, number);
+
+// Делаем вывод по условию.
+// синтаксис if(key) , при типе bool, по-умолчанию true 
+if(key)
+{
+    System.Console.WriteLine($"Ура! Число {number} есть в массиве!");
+}
+else
+{
+    System.Console.WriteLine($"Увы, числа {number} в массиве нет");
+}
