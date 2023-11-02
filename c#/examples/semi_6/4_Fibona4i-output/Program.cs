6902572023413bbacec69d0f7ec3c9483789f4f2
hatter <hatter@complat.ru>
2023-11-02 18:43:17 +0300

    add semi_6 4th task Fibona4i-output:

---
+++ c#/examples/semi_6/4_Fibona4i-output/Program.cs
@@ -0,0 +1,22 @@
+﻿//Код выводит числа Фибоначи без использования рекурсии
+
+int ReadInt(string text)
+{ 
+    Console.WriteLine("Введите число:"); 
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void Fib(int N)
+{
+Console.Write("0 1 ");
+int num1 = 0;
+int num2 = 1;
+
+for (int i = 0; i < N - 2; i++)
+    {
+    int res = num1 + num2; Console.Write(res + " ");
+    num1 = num2; num2 = res;
+    }
+}
+
+Fib(ReadInt(" "));
