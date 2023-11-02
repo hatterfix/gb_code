cfc138d18c06cd7781b7ab5c2384f0aed3e9b1ed
hatter <hatter@complat.ru>
2023-11-02 19:04:49 +0300

    add semi_6 5th task copy-of-array:

---
+++ c#/examples/semi_6/5_copy-of-array/Program.cs
@@ -0,0 +1,44 @@
+﻿// Код создаёт копию массива с помощью поэлементного копирования.
+
+int[] generateArray (int sizeArray , int leftRange , int rightRange)
+{
+int[] tmpArray = new int[sizeArray] ;
+Random rand = new Random() ;
+
+// Цикл для заполнения массива случайными числами из указанного диапазона
+
+for (int i = 0 ; i < tmpArray.Length ; i++)
+    {
+        tmpArray[i] = rand.Next(leftRange , rightRange + 1) ; // т к rand не обработает правое значение диапазона
+    }
+    return tmpArray ;
+}
+
+
+void arrayPrint(int[] array)
+{
+     System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+
+int[] Copy(int[] oldArray)
+{
+int[] newArray = new int[oldArray.Length];
+
+for (int i = 0; i < oldArray.Length; i++) 
+    newArray[i] = oldArray[i];
+
+return newArray;
+}
+
+//--------------------------------------------------------------------------------------------------------------------
+
+int[] myArray = generateArray(5, -5, 5);
+int[] copyArray = Copy (myArray);
+int[] secondArray = myArray;
+
+myArray[0] = 100 ; // Для демонстрации, что функция копирования не обработает постусловия
+
+arrayPrint(myArray); 
+arrayPrint(copyArray); 
+arrayPrint(secondArray); 
