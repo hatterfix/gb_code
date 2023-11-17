457c8314f78ca7e977ce6eebf8bce388510bcf10
Kirill Krivospitskiy <hatter@complat.ru>
2023-11-17 15:13:29 +0300

    add semi 9 exam 1st task:

---
+++ c#/examples/semi_9-exam/1_recur_N-to-1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Код ыведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+
+//Функция для ввода числа N:
+
+int ReadInt(string text)
+
+{
+        int result;
+        while (!int.TryParse(System.Console.ReadLine(), out result) || result <= 0)
+        {
+            System.Console.WriteLine("Ошибка ввода. Пожалуйста, введите натуральное число > 0.");
+            System.Console.Write(text);
+        }
+        return result;
+}
+
+void ShowNum(int num) //Рекурсивная функция - вызывает саму себя
+{
+    if(num == 0) return ; //  Условие для остановки вызова функции, доходим до 0
+    System.Console.Write(num + ", "); // Выводим первое число
+    ShowNum(num - 1);// Запускаем рекурсию, уменьшая число на 1
+}
+
+//-----------------------------------------------------
+
+System.Console.WriteLine("Введите число N:");
+
+int number = ReadInt("Введите натуральное число N: ");
+
+ShowNum (number);
