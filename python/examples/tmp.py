46022fa4e1cadaefec831ea0aeab8bfe0a013dec
Kirill Krivospitskiy <hatter@complat.ru>
2023-12-14 02:54:53 +0300

    add round  descr:

--- python/examples/tmp.py
+++ python/examples/tmp.py
@@ -1,8 +1,6 @@
 ## tmp.py
-c = 5.86
-print(type(c))
-print(c)
-#меняем тип переменной на int
-a = int(input("Введите число: "))
+a = 7.86672162
+b = 5.42767261
 print(type(a))
-print(a)
+print(type(b))
+print(round(a*b, 3))
