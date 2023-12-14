846529d45aa7ff3575c93a6874ef215d0500c606
Kirill Krivospitskiy <hatter@complat.ru>
2023-12-14 12:56:23 +0300

    add flag method  descr:

--- README.md
+++ README.md
@@ -377,4 +377,24 @@ else:
 
 print(i)
 ```
+
+- Метод флажка flag = true / false:
+
+Пример: 
+```sh
+n = int(input("Введите число: "))
+flag = True
+i = 2
+
+while flag:
+     # если остаток при делении числа п на 1 равен 0
+    if n % i == 0:
+        flag = False
+        print(i)
+# делитьель числа не может превышать введенное введенное число деленое на 2
+    elif i > n // 2: 
+        print(n)
+        flag = False
+    i += 1
+```
 </details>
--- python/examples/tmp.py
+++ python/examples/tmp.py
@@ -1,11 +1,15 @@
 ## tmp.py
-i = 6
-while i < 5:
-    if i == 3:
-        break
-    i = i + 1
-else:
-    print("Пожалуй")
-    print('хватит )')
+n = int(input("Введите число: "))
+flag = True
+i = 2
 
-print(i)
+while flag:
+     # если остаток при делении числа п на 1 равен 0
+    if n % i == 0:
+        flag = False
+        print(i)
+# делитьель числа не может превышать введенное введенное число деленое на 2
+    elif i > n // 2: 
+        print(n)
+        flag = False
+    i += 1
