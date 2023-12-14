3e26a503dfb183a691d12cd2c91eb3947c036bc8
Kirill Krivospitskiy <hatter@complat.ru>
2023-12-14 16:42:53 +0300

    add part of semi_1 examples:

---
+++ python/examples/semi_1/2_var-types.py
@@ -0,0 +1,11 @@
+# name = input("Введите имя: ")
+# age = input("Введите свой возраст: ")
+# print(name + " " + age)
+# print(f'Привет, {name}! Тебе {age} лет.') # f-строки или форматированный вывод
+
+name = 'Sidor'
+print(type(name)) # <class 'str'>
+number = 123
+print(type(number)) # <class 'int'>
+float_number = 334.192
+print(type(float_number)) # <class 'float'>
---
+++ python/examples/semi_1/3_incloded-func.py
@@ -0,0 +1,8 @@
+# Встроенные функции(int, float, str)
+print(int("234") + 6)
+print(int(3.99))
+print(int("234"))
+# print(int("23 4")) ValueError: invalid literal for int() with base 10: '23 4'
+print(float(56))
+print(str(45.56) * 2)
+print(str(12) * 2)
