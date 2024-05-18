6f3eef4fb36123b8a77dc24f1678bab3bdd00d92
Kirill Krivospitskiy <hatterfix@gmail.com>
2024-05-18 18:05:32 +0300

    update displayContact void:

--- java/phonebook_attest/PhoneBook.java
+++ java/phonebook_attest/PhoneBook.java
@@ -10,4 +10,12 @@ public class PhoneBook {
         // добавляет номер телефона к имени сузествующего контакта
         contacts.computeIfAbsent(name, k -> new HashSet<>()).add(phone);
     }
+
+    // Выводим контакты на экран, по убыванию количества номеров
+    public void displayContacts() {
+        // Преобразуем записи в  телефонной книге в поток , затем сортируем
+        contacts.entrySet().stream()
+                .sorted((e1, e2) -> Integer.compare(e2.getValue().size(), e1.getValue().size()))
+                .forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
+    }
 }
