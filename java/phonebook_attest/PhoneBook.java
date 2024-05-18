f05c0150abeec8b902175d6fac1182395f1eb751
Kirill Krivospitskiy <hatterfix@gmail.com>
2024-05-18 17:58:57 +0300

    update addContact void:

--- java/phonebook_attest/PhoneBook.java
+++ java/phonebook_attest/PhoneBook.java
@@ -4,4 +4,10 @@ public class PhoneBook {
     // Храним контакты в виде "Имя" -> "Номера телефонов"
     private Map<String, Set<String>> contacts = new HashMap<>();
 
+    // Добавляет контакт в телефонную книгу
+    public void addContact(String name, String phone) {
+        // Метод computeIfAbsent добавляет контакт или обновляет существующие
+        // добавляет номер телефона к имени сузествующего контакта
+        contacts.computeIfAbsent(name, k -> new HashSet<>()).add(phone);
+    }
 }
